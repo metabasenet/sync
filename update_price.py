f400c41b413b1cc9aebc937be42d48988c6fe7da
flybird000 <flybird000@outlook.com>
2022-06-22 16:56:45 +0800

    modify response error

--- update_price.py
+++ update_price.py
@@ -8,39 +8,43 @@ import config
 import pymysql
 
 def Update():
-    conn = pymysql.connect(host=config.host, port=config.port, user=config.user, password=config.password, db=config.db)
-    cursor = conn.cursor()
-    #url = 'https://dncapi.fxhapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=50&webp=1'
-    url ='https://shangqingdong.work/bsc/'
-    response = requests.get(url=url)
-    objs = json.loads(response.text)
-    sqls = []
-    for obj in objs["data"]:
-        if obj["name"] == 'BTC':
-            sql = 'update quotations set price = %s,price24h = %s where tradePairId = "BTC/USDT"' \
-                % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
-            sqls.append(sql)
-        elif obj["name"] == "ETH":
-            sql = 'update quotations set price = %s,price24h = %s where tradePairId = "ETH/USDT"' \
-                % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
-            sqls.append(sql)
-        elif obj["name"] == "BNB":
-            sql = 'update quotations set price = %s,price24h = %s where tradePairId = "BNB/USDT"' \
-                % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
-            sqls.append(sql)
-        elif obj["name"] == "TRX":
-            sql = 'update quotations set price = %s,price24h = %s where tradePairId = "TRX/USDT"' \
-                % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
-            sqls.append(sql)
-        elif obj["name"] == "XRP":
-            sql = 'update quotations set price = %s,price24h = %s where tradePairId = "XRP/USDT"' \
-                % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
-        else:
-            pass
-    for sql in sqls:
-        print(sql)
-        cursor.execute(sql)
-    conn.commit()
+    try:
+        conn = pymysql.connect(host=config.host, port=config.port, user=config.user, password=config.password, db=config.db)
+        cursor = conn.cursor()
+        url = 'https://dncapi.fxhapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=50&webp=1'
+        #url ='https://shangqingdong.work/coin/'
+    
+        response = requests.get(url=url)
+        objs = json.loads(response.text)
+        sqls = []
+        for obj in objs["data"]:
+            if obj["name"] == 'BTC':
+                sql = 'update quotations set price = %s,price24h = %s where tradePairId = "BTC/USDT"' \
+                    % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
+                sqls.append(sql)
+            elif obj["name"] == "ETH":
+                sql = 'update quotations set price = %s,price24h = %s where tradePairId = "ETH/USDT"' \
+                    % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
+                sqls.append(sql)
+            elif obj["name"] == "BNB":
+                sql = 'update quotations set price = %s,price24h = %s where tradePairId = "BNB/USDT"' \
+                    % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
+                sqls.append(sql)
+            elif obj["name"] == "TRX":
+                sql = 'update quotations set price = %s,price24h = %s where tradePairId = "TRX/USDT"' \
+                    % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
+                sqls.append(sql)
+            elif obj["name"] == "XRP":
+                sql = 'update quotations set price = %s,price24h = %s where tradePairId = "XRP/USDT"' \
+                    % (obj["current_price_usd"],(obj["current_price_usd"] * 100) / (100 + obj["change_percent"]))
+            else:
+                pass
+        for sql in sqls:
+            print(sql)
+            cursor.execute(sql)
+        conn.commit()
+    except Exception as r:
+        print('%s' %(r))
 
 if __name__ == '__main__':
     while True:
