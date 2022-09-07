f80b25c85bed4bac71dee292bea05e3becd993f9
flybird000 <flybird000@outlook.com>
2022-09-07 17:19:27 +0800

    modify address

--- bridge.py
+++ bridge.py
@@ -9,8 +9,8 @@ import requests
 conn = pymysql.connect(host=config.host, port=config.port, user=config.user, password=config.password, db=config.db)
 
 
-bridge_addr = '1231kgws0rhjtfewv57jegfe5bp4dncax60szxk8f4y546jsfkap3t5ws'
-
+#bridge_addr = '1231kgws0rhjtfewv57jegfe5bp4dncax60szxk8f4y546jsfkap3t5ws'
+bridge_addr = '20m07b974wdrzrwtxghteg223fae28awc3srnvp4ypxg87p0mf5hrtr6t'
 
 except_addr = '000000000000000000000000000000000000000000000000000000000'
 
