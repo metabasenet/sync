f84174aef7d1d1f8a07deeb0790dbf2455a260cf
zhang240320 <zhang240320@gmail.com>
2024-04-09 17:03:52 +0800

    update erc20 balance

--- mntHistorySync.js
+++ mntHistorySync.js
@@ -206,6 +206,34 @@ function getAddressCallback(err, result) {
     }
 }
 
+//update erc20 balance 
+sqlHelper.init();
+const selectErc20AddressSql = "SELECT  DISTINCT contractAddress,address  from (SELECT DISTINCT contractAddress,`from` AS address FROM `transaction_erc20` UNION SELECT DISTINCT contractAddress,`to` AS address FROM `transaction_erc20`) A";
+sqlHelper.readDatabase(selectErc20AddressSql, getErc20AddressCallback);
+async function getErc20AddressCallback(err, result) {
+    if (err) {
+        console.log(err.message);
+    } else {
+        for (let i in result) {
+            try {
+                const ERC20ABi = JSON.parse(fs.readFileSync("./abi/erc20.json", "utf8"));
+                const contract = new ethers.Contract(result[i].contractAddress, ERC20ABi, provider);
+                //update ERC20 balance
+                let address = result[i].address.replace("0x000000000000000000000000", "0x");
+                let balance = await contract.balanceOf(address);
+                console.log(balance)
+                if (balance !== null) {
+                    let insertSql = `REPLACE  INTO erc20_balance(address,contractAddress, balance, updateTime) VALUES ('${address}','${result[i].contractAddress}', '${balance}', NOW())`;
+                    sqlHelper.writeDatabase(insertSql, new Date());
+                }
+            } catch (ex) {
+                console.log(ex.message)
+            }
+        }
+    }
+}
+
+
 async function bulkCreateBlock(blockInfoArray) {
     await Block.bulkCreate(blockInfoArray).then(() => {
         console.log("Block save successfully!");
